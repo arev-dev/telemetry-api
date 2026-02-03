299fb3d0079a8becb2e1071cd98056e728c6d4cb
Daniel Rodriguez <arevdanielrodriguez@gmail.com>
2026-02-02 20:23:29 -0600

    change smtp flag

--- src/mail.service.ts
+++ src/mail.service.ts
@@ -7,7 +7,7 @@ export class MailService {
   private transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false, // STARTTLS
+    secure: true, // STARTTLS
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
