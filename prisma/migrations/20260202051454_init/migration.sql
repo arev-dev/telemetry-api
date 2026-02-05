6784f2b6a150ab9de146f53b0a3d0b6d35561169
Daniel Rodriguez <arevdanielrodriguez@gmail.com>
2026-02-04 18:14:07 -0600

    clean prisma

--- prisma/migrations/20260202051454_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "TelemetryEvent" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "eventType" TEXT NOT NULL,
-    "platform" TEXT,
-    "gameVersion" TEXT,
-    "message" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
